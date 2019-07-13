d95ff5e15a90e44a4b78526525339a4c10252cb2
mandyphg <44054536+mandyphg@users.noreply.github.com>
2019-07-13 09:54:35 +0800

    Create minimumBribes.py

---
+++ minimumBribes.py
@@ -0,0 +1,57 @@
+'''
+https://www.hackerrank.com/challenges/new-year-chaos/problem
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+
+
+    moves_no = 0
+
+    for i in range(len(q)):
+        move = q[i] - i - 1
+        if move > 2:
+            print("Too chaotic")
+            return
+    
+    for c1 in range(len(q)):    
+
+        for c2 in range(len(q)):
+
+            c3 = len(q) - c2 - 1
+            move = q[c3] - c3 - 1
+                
+            if c3>0:                
+                if q[c3] < q[c3-1]:
+                    moves_no += 1
+                    q[c3],q[c3-1] = q[c3-1]*1,q[c3]*1
+                    
+        move_check = 0        
+        for i in range(len(q)):
+            move = q[i] - i - 1
+            move_check += (move >0)*1
+
+        if move_check == 0:
+            print(moves_no)
+            return
+    print(moves_no)
+            
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
