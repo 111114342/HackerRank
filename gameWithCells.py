2a3b932f8a7d6ff1dfd9582692879701ea040245
mandyphg <44054536+mandyphg@users.noreply.github.com>
2019-07-14 11:45:25 +0800

    Create gameWithCells.py

---
+++ gameWithCells.py
@@ -0,0 +1,14 @@
+'''
+https://www.hackerrank.com/challenges/game-with-cells/problem
+'''
+
+
+def gameWithCells(n, m):
+    
+    n_drop = int((n+1)/2)
+    m_drop = int((m+1)/2)
+    
+    result = n_drop * m_drop
+    
+    return result
+    
